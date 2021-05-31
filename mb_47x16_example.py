39fd3a159c1523ed914b26482a10cb3abc365167
MarksBench <82638756+MarksBench@users.noreply.github.com>
2021-05-31 14:05:13 -0500

    Update mb_47x16_example.py
    Updated comments at top.

--- mb_47x16_example.py
+++ mb_47x16_example.py
@@ -1,7 +1,32 @@
 """
 mb_47x16_example.py
-
+Example MicroPython script for Microchip 47x16 EERAM with RP2040 (Raspberry Pi Pico)
+(Should work with other MicroPython-capable devices with hardware of software I2C)
+Author: mark@marksbench.com
+Version: 0.1, 2021-05-31
+**NOTE: There is no guarantee that this software will work in the way you expect (or at all).
+**Use at your own risk.
+To use:
+- Upload the mb_47x16.py file to your board (the same location where your MicroPython scripts
+  run from, or the /lib folder if applicable).
+- Connect the 47L16 (the "C" version is 5V!) to the Pi Pico (RP2040).
+- For testing (and to use this example script without modification) I suggest the following:
+  
+        47L16    |    Pi Pico
+        1(Vcap)  |    NC. Connect instead to +ve of >10uF capacitor and -ve of cap to GND
+        2(A1)    |    NC (internally pulled-down)
+        3(A2)    |    NC (internally pulled-down)
+        4(Vss)   |    GND, (Pin 38)
+        5(SDA)   |    GP0(Pin 1)
+        6(SCL)   |    GP1(Pin 2)
+        7(HS)    |     NC (internally pulled-down)
+        8(Vcc)   |   3V3 OUT, (Pin 36)
+        
+- To write a value: memory.write_byte(address, value)
+- To read a value: value = memory.read_byte(address)
 - You should get an error if the address or value is out of range.
+- To store contents of SRAM to EEPROM: memory.store_to_EEPROM()
+- To recall contents of EEPROM to SRAM: memory.recall_from_EEPROM()
 """
 
 
