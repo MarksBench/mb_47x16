2b619689ce33d83e289510aa49de4379d617fd98
MarksBench <82638756+MarksBench@users.noreply.github.com>
2021-05-31 13:56:35 -0500

    Update mb_47x16_example.py
    Removed incorrect information in comments at top

--- mb_47x16_example.py
+++ mb_47x16_example.py
@@ -1,28 +1,6 @@
 """
 mb_47x16_example.py
-Example MicroPython script for 64Mbit SPI PSRAM with RP2040 (Raspberry Pi Pico)
-(Should work with other MicroPython-capable devices with hardware of software SPI)
-Author: mark@marksbench.com
-Version: 0.1, 2021-05-26
-**NOTE: There is no guarantee that this software will work in the way you expect (or at all).
-**Use at your own risk.
-To use:
-- Upload the mb_PSRAM_64Mb_SPI.py file to your board (the same location where your MicroPython scripts
-  run from, or the /lib folder if applicable).
-- Connect the PSRAM to the Pi Pico (RP2040), for testing (and to use this example script
-  without modification) I suggest the following:
-  
-        PSRAM    |    Pi Pico
-        1(/CE)   |    GP1 (Pin 2)
-        2(SO)    | GP4, (SPI0 RX, Pin 6)
-        3(SIO[2])|      NC
-        4(Vss)   |    GND, (Pin 38)
-        5(SI)    | GP7, (SPI0 TX, Pin 10)
-        6(SCK)   | GP6, (SPI0 SCK, Pin 9)
-        7(SIO[3])|      NC
-        8(Vcc)   |   3V3 OUT, (Pin 36)
-- To write a value: memory.write_byte(address, value)
-- To read a value: value = memory.read_byte(address)
+
 - You should get an error if the address or value is out of range.
 """
 
